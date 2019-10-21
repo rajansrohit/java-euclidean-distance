8f11c3fd0162e06baac2458393be29f5ca4a83d9
Rohit Rajan <41314110+rajansrohit@users.noreply.github.com>
2019-10-21 09:03:43 -0500

    Update eucdist.java

--- eucdist.java
+++ eucdist.java
@@ -3,9 +3,15 @@ public class eucdist{
   public static void main(String[] args){
     Scanner scanner = new Scanner(System.in);
     
-    System.out.println();
-    System.out.println();
-    System.out.println();
-    System.out.println();
+    System.out.println("x value of first coordinate:");
+    int x1 = scanner.nextInt();
+    System.out.println("y value of first coordinate:");
+    int y1 = scanner.nextInt();
+    System.out.println("x value of second coordinate:");
+    int x2 = scanner.nextInt();
+    System.out.println("x value of second coordinate:");
+    int y2 = scanner.nextInt();
+    
+    
   }
 }
