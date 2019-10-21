b6c858f8069fec605489b9142529887e219df960
Rohit Rajan <41314110+rajansrohit@users.noreply.github.com>
2019-10-21 09:02:07 -0500

    Update eucdist.java

--- eucdist.java
+++ eucdist.java
@@ -1,1 +1,11 @@
-
+public class eucdist{
+  
+  public static void main(String[] args){
+    Scanner scanner = new Scanner(System.in);
+    
+    System.out.println();
+    System.out.println();
+    System.out.println();
+    System.out.println();
+  }
+}
